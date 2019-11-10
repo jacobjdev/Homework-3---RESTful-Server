e2cacc2f1013a4787fed388d5a497035b5878705
Jacob Jensen <jacob@jacobj.org>
2019-11-09 20:12:30 -0600

    Added an JSON to XML package for part of getting an B or an A on the assignment, added the different skeleton API calls and addd package.json information.

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 // Built-in Node.js modules
-var fs = require('fs')
-var path = require('path')
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
 
 // NPM modules
-var express = require('express')
-var sqlite3 = require('sqlite3')
+var express = require('express');
+var sqlite3 = require('sqlite3');
+var JSONtoXML = require('js2xmlparser');
 
 
 //var public_dir = path.join(__dirname, 'public');
 
 var db_filename = path.join(__dirname, 'database', 'stpaul_crime.sqlite3');
-
+app.use(bodyParser.urlencoded({extended:true}));
 var app = express();
 var port = 8000;
 
@@ -21,4 +23,24 @@ var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
     else {
         console.log('Now connected to ' + db_filename);
     }
+});
+
+
+app.get('/codes', (req,res) => {
+
+});
+
+
+app.get('/neighborhoods',(req,res) => {
+
+});
+
+
+app.get('/incidents', (req,res) => {
+
+});
+
+app.put('/new-incident', (req,res) =>{
+
+
 });
