941620c5a56b75cf984b81d015f44bc88e6e7bcc
Jacob Jensen <jacob@jacobj.org>
2019-11-10 16:01:04 -0600

    Made progress on trying to get the codes to return something, need to figure out how to get it converted over, there should be an easier way to do this but not too sure how?

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ var JSONtoXML = require('js2xmlparser');
 //var public_dir = path.join(__dirname, 'public');
 
 var db_filename = path.join(__dirname, 'database', 'stpaul_crime.sqlite3');
-app.use(bodyParser.urlencoded({extended:true}));
 var app = express();
 var port = 8000;
+app.use(bodyParser.urlencoded({extended:true}));
 
 var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
     if (err) {
@@ -27,7 +27,20 @@ var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
 
 
 app.get('/codes', (req,res) => {
-
+    var JsonToSend = {codes:[]};
+    var stringToStringify = "";
+    //want the key to be the code string and the value being what the type is
+    //SELECT * FROM Codes ORDER BY code
+    db.each("SELECT * FROM Codes ORDER BY code", (err, row) =>{
+        // var newJSONEntry = {code: row.code, incident_type:row.incident_type}
+        // JsonToSend.codes.push(newJSONEntry);
+        stringToStringify += "\""+ parseInt(row.code) +"\""+ ":" +"\""+ row.incident_type + "\""+ ",";
+    }, () =>{
+        // console.log(JSON.parse(stringToStringify));
+        stringToStringify = stringToStringify.substring(0,stringToStringify.length-1);
+        console.log(stringToStringify)
+        console.log(JSON.parse(stringToStringify));
+    });
 });
 
 
@@ -44,3 +57,6 @@ app.put('/new-incident', (req,res) =>{
 
 
 });
+
+console.log('Now listening on port ' + port);
+var server = app.listen(port);
