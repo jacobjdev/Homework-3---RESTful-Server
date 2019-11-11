540f0eb490702e2169cd386b56dcdf82563c0722
Jacob Jensen <jacob@jacobj.org>
2019-11-10 22:02:47 -0600

    Adding different test ideas and working on the put case.

--- server.js
+++ server.js
@@ -27,20 +27,45 @@ var db = new sqlite3.Database(db_filename, sqlite3.OPEN_READONLY, (err) => {
 
 
 app.get('/codes', (req,res) => {
-    //var JsonToSend = {codes:[]};
+    var JsonToSend = {codes:[]};
     var stringToStringify = "{ ";
     //want the key to be the code string and the value being what the type is
     //SELECT * FROM Codes ORDER BY code
     db.each("SELECT * FROM Codes ORDER BY code", (err, row) =>{
-        // var newJSONEntry = {code: row.code, incident_type:row.incident_type}
-        // JsonToSend.codes.push(newJSONEntry);
-        stringToStringify += "\""+ parseInt(row.code) +"\""+ ":" +"\""+ row.incident_type + "\""+ ",";
+        var newCode = parseInt(row.code);
+        var newIncidntType = row.incident_type;
+        //why no newJSONENTRY = {parseInt(row.code):newIncidentType}
+        var newJSONEntry = {newCode: newIncidntType};
+        JsonToSend.codes.push(newJSONEntry);
+        // stringToStringify += "\""+ parseInt(row.code) +"\""+ ":" +"\""+ row.incident_type + "\""+ ",";
     }, () =>{
         // console.log(JSON.parse(stringToStringify));
-		stringToStringify = stringToStringify.substring(0,stringToStringify.length-1)+ "}";
-		res.type('json').send(JSON.parse(stringToStringify));
+		// stringToStringify = stringToStringify.substring(0,stringToStringify.length-1)+ "}";
+		// res.type('json').send(JSON.parse(stringToStringify));
         //console.log(stringToStringify)
-        console.log(JSON.parse(stringToStringify));
+        // console.log(JSON.parse(stringToStringify));
+        console.log(JSON.stringify(JsonToSend));
+    });
+});
+
+app.get('/codes2', (req,res) => {
+    var codesString = "{ ";
+    db.each('SELECT * FROM Codes ORDER BY code',(err, row) =>{
+        //console.log(rows);
+        codesString += "\"" + parseInt(row.code) + "\"" + ":" + "\"" + row.incident_type + "\"" + ",";
+        codesString = codesString.substring(0,codesString.length-1)+"}";
+    }, () =>{
+        res.type('json').send(JSON.parse(codesString))
+    });
+    // console.log(JSON.stringify(JsonToSend));
+});
+app.get('/codes3', (req,res)=> {
+    var JSONStuff;
+    db.each('SELECT * FROM Codes ORDER BY codes', (err, row) =>{
+
+        JSONStuff = JSONStuff + row.code
+    }, () => {
+
     });
 });
 
@@ -84,9 +109,31 @@ app.get('/incidents', (req,res) => {
 	
 
 });
-
+//curl -X PUT -d "case_number=[NUMBER]&date_time=2019-10-26T02:50:13.000&code=643&incident=Auto Theft&police_grid=62&neighborhood_num=12&block=2X RAYMOND PL" http://localhost:8000/new-incident
 app.put('/new-incident', (req,res) =>{
-
+    //WHAT FIELDS ARE REQUIRED TO PROCESS?  OR CAN JUST BE A CASE NUMBER AND THATS IT>
+    //how to add error checking to make sure contains all the required fields?
+    console.log(req.body)
+    var duplicateCaseNumber = false;
+    var case_number = req.body.case_number;
+    db.each("SELECT case_number FROM Incidents", (err, row) =>{
+        if(row.case_number === case_number){
+            duplicateCaseNumber = true;
+        }
+    }, () => {
+    if(duplicateCaseNumber){
+        res.status(500).send('error:case ID already exists');
+    }else{
+        // do the processing to upload
+        var newCaseNumber = req.body.case_number;
+        var newDateTime = req.body.date_time;
+        var newCode = req.body.code;
+        var newIncident = req.body.incident;
+        var newPoliceGrid = req.neighborhood_num;
+        var newBlock = req.block;
+        //how to upload it?????????
+    }
+    });
 
 });
 
