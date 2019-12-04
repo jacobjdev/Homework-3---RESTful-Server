ec6ed93ed4ca23876163fdc5dab9131e0f272a46
Jacob Jensen <jacob@jacobj.org>
2019-12-04 17:03:29 -0600

    Starting to get the pieces in place for part 2 of the project.

---
+++ WebsiteFrontEnd.js
@@ -0,0 +1,14 @@
+var app;
+
+function Init(){
+    // so main application page stuff goes in here?
+    app = new Vue({
+        el: "#app",
+        data: {
+
+        }
+    })
+
+}
+
+//have separate function down here for api processing call stuff?
